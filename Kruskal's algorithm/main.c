0653b05dc225fcc1080a4631a39724cd954c2ab4
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-04 01:00:45 +0700

    Update main.c

--- Kruskal's algorithm/main.c
+++ Kruskal's algorithm/main.c
@@ -1,75 +1,50 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-int convert_from_greater_to_less(int b1, int b2, char input[]) {
-    int result = 0;
-    for (int i = 0; i < input.length(); i++) {
-        
-    }
-    return result;
+void is_bad_input(int num_vertexes, int num_edges, FILE *output_file) {
+	if (num_vertexes < 0 || num_vertexes > 5000) {
+		fprintf(output_file, "bad number of vertices");
+		exit(0);
+	}
+	if (num_edges < 0 || num_edges >(num_vertexes * (num_vertexes - 1)) / 2) {
+		fprintf(output_file, "bad number of edges");
+		exit(0);
+	}
 }
 
-int convert_from_less_to_greater(int b1, int b2, char input[]) { //b1 is less 
-    int result = 0;
-    for (int i = 0; i < input.length(); i++) {
-        result += input[i] * pow(b1, input.length() - i);
-    }
-    return result;
-}
+int main() {
+	FILE *input_file = fopen("in.txt", "r");
+	FILE *output_file = fopen("out.txt", "a");
 
-int pow (int a, int b) {
-    
-}
+	if (input_file == NULL) {
+		fputs("no such file", output_file);
+		exit(0);
+	}
 
-int main()
-{
-    FILE *myfile; // указатель на файл 
-    myfile = fopen("in.txt", "r");
-    
-    int b1, b2;
-    fgets(b1,) //TODO чтение переменных инт
-    char mystring[13]; 
-    if(b1 > 1 && b2 > 1 && b1 < 17 && b2 < 17) {
-        fgets(mystring ,13, myfile); //остановится ли если символов меньше 12
-        char first[12];  //инициализированно ли 0 ? 
-        char second[11];
-        int flag = 0;
-        for (int i = 0; mystring[i] != '.' ; i++) {
-                if(mystring[i] >= 'a' && mystring[i] <= 'g' && b1 > 10) {
-                    first[i] = 'g' - (int) (mystring[i]); //нужно ли приведение типа
-                } else if(mystring[i] >= 'A' && mystring[i] <= 'G' && b1 > 10) {
-                    first[i] = 'G' - (int) (mystring[i]);
-                } else if(mystring[i] >= '0' && mystring[i] <= '9') {
-                    first[i] = mystring[i];
-                } else {
-                    //cout << "bad input" 
-                    break; // выйдет ли из основного ифа ? 
-                }
-                if(mystring[i+1] == '.') {
-                flag = i + 1;
-            
-            }
-        }
-        
-        if(flag != 0) { //функция для переписи, аргументы second/first, mystring. Эта для second
-            for (int i = flag; i < mystring.length(); i++) {
-                if(mystring[i] >= 'a' && mystring[i] <= 'g' && b1 > 10) {
-                    secong[i] = 'g' - (int) (mystring[i]); //нужно ли приведение типа
-                } else if(mystring[i] >= 'A' && mystring[i] <= 'G' && b1 > 10) {
-                    second[i] = 'G' - (int) (mystring[i]);
-                } else if(mystring[i] >= '0' && mystring[i] <= '9') {
-                    second[i] = mystring[i];
-                } else {
-                    //cout << "bad input" 
-                    break; // выйдет ли из основного ифа ? 
-                }
-            }
-        }
-    else {
-        //cout << "bad input"
-    }
-    } else{
-        //cout << "bad input"
-    }
-    fclose(myfile);
-    return 0;
+	int num_vertices, num_edges;
+	if (fscanf(input_file, "%d %d", &num_vertices, &num_edges) == EOF) {
+		fprintf(output_file, "bad number of lines");
+		exit(0);
+	}
+	is_bad_input(num_vertices, num_edges, output_file);
+
+	graphNode *graph = createGraph(num_vertices);
+
+	int beg, end, len;
+	for (int i = 0; i < num_edges; i++) {
+		if (fscanf(input_file, "%d %d %lld", &beg, &end, &len) == EOF) {
+			fprintf(output_file, "bad number of lines");
+			exit(0);
+		}
+		if (len > (long long)INT_MAX || len < 0) {
+			fprintf(output_file, "bad length");
+			exit(0);
+		}
+		addEdge(beg, end, len, graph);
+	}
+	
+	fclose(input_file);
+	fclose(output_file);
+	return 0;
 }
